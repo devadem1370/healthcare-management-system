f7beecf6e635da3da610d7483c4cde017dd497ea
Adil Imran <devadem1370@gmail.com>
2025-06-17 04:39:21 -0700

    complete patient registeration

---
+++ app/patients/[userId]/register/page.tsx
@@ -0,0 +1,46 @@
+import RegisterForm from "@/components/forms/RegisterForm";
+import { getUser } from "@/lib/actions/patient.actions";
+import Image from "next/image";
+
+type SearchParamProps = {
+  params: {
+    userId: string;
+  };
+};
+
+const Register = async (props: SearchParamProps) => {
+  const { userId } = props.params;
+
+  // Fetch the user data
+  const user = await getUser(userId);
+
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="container remove-scrollbar">
+        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+          <Image
+            src="/assets/icons/logo.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="h-10 mb-12 w-fit"
+          />
+
+          <RegisterForm user={user} />
+
+          <p className="py-12 copyright">© 2024 CarePluse</p>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/register.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[390px]"
+      />
+    </div>
+  );
+};
+
+export default Register;
--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -105,28 +105,28 @@ const RenderInput = ({ field, props }: { field: any; props: CustomProps }) => {
           </div>
         </FormControl>
       );
-    // case FormFieldType.DATE_PICKER:
-    //   return (
-    //     <div className="flex border rounded-md border-dark-500 bg-dark-400">
-    //       <Image
-    //         src="/assets/icons/calendar.svg"
-    //         height={24}
-    //         width={24}
-    //         alt="user"
-    //         className="ml-2"
-    //       />
-    //       <FormControl>
-    //         <ReactDatePicker
-    //           showTimeSelect={props.showTimeSelect ?? false}
-    //           selected={field.value}
-    //           onChange={(date: Date) => field.onChange(date)}
-    //           timeInputLabel="Time:"
-    //           dateFormat={props.dateFormat ?? "MM/dd/yyyy"}
-    //           wrapperClassName="date-picker"
-    //         />
-    //       </FormControl>
-    //     </div>
-    //   );
+    case FormFieldType.DATE_PICKER:
+      return (
+        <div className="flex border rounded-md border-dark-500 bg-dark-400">
+          <Image
+            src="/assets/icons/calender.svg"
+            height={24}
+            width={24}
+            alt="user"
+            className="ml-2"
+          />
+          <FormControl>
+            <ReactDatePicker
+              showTimeSelect={props.showTimeSelect ?? false}
+              selected={field.value}
+              onChange={(date: Date | null) => field.onChange(date)}
+              timeInputLabel="Time:"
+              dateFormat={props.dateFormat ?? "MM/dd/yyyy"}
+              wrapperClassName="date-picker"
+            />
+          </FormControl>
+        </div>
+      );
     case FormFieldType.SELECT:
       return (
         <FormControl>

57fa6b8e6b361c09c2344638d66b717cb80baf7c
Adil Imran <devadem1370@gmail.com>
2025-06-20 03:39:42 -0700

    create passKeyModal

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import { PatientForm } from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PassKeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
 
-const Home = () => {
+const Home = ({searchParams}: SearchParamProps) => {
+
+  const isAdmin = searchParams.admin === "true";
 
   return (
     <div className="flex h-screen max-h-screen">
-      {/* {isAdmin && <PasskeyModal />} */}
+      {isAdmin && <PasskeyModal />}
 
       <section className="container my-auto remove-scrollbar">
         <div className="sub-container max-w-[496px]">
